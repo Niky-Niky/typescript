f8ee2ac36ae7dabf7a6071d4227fd2fbba1cda19
Niky-Niky <nik333nik888@gmail.com>
2024-03-13 09:26:33 +0100

    commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+const userName = 'Bro';
+let id;
+const coords = [54.41912, 171.26221];
+const rgb = [138, 0, 200];
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const userName: string = 'Bro'
+
+
+let id: number | string;
+
+type NumArr = {
+    number: number[]
+}
+
+const coords: [number, number] = [54.41912, 171.26221]
+
+const rgb: [number, number, number] = [138, 0, 200]
